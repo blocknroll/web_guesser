c0393ba44c60e8a2caf917899b7c21fc580208c1
Fred Block <info@fredblock.com>
2015-02-02 16:06:31 -0700

    I4: colorize the output.

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,5 +1,44 @@
-# V3: using settings for 'number'
-#
+# I4: add colors
+
+require 'sinatra'
+require 'sinatra/reloader'
+
+number = rand(100)
+
+def check_guess(guess, number)
+  if (guess - number) > 5
+    {"message" => "Way too high!", "background" => "red"}
+
+  elsif guess > number
+    {"message" => "Too high!", "background" => "orange"}
+
+  elsif (number - guess) > 5
+    {"message" => "Way too low!", "background" => "red"}
+
+  elsif guess < number
+    {"message" => "Too low!", "background" => "orange"}
+
+  elsif guess == number
+    {"message" => "You are correct! <p>The secret number is #{number}.</p>", "background" => "green"}
+  end
+
+end
+
+get '/' do
+  guess = params['guess'].to_i
+  messages = check_guess(guess, number)
+  erb :index, :locals => {:number  => number,
+                          :messages => messages }
+end
+
+
+
+
+
+
+
+# # I3: refactor using settings for 'number'
+
 # require 'sinatra'
 # require 'sinatra/reloader'
 #
@@ -29,39 +68,49 @@
 # end
 
 
-# V2
+
+
+
+
+
+
+# # I2
+
+# require 'sinatra'
+# require 'sinatra/reloader'
 #
-require 'sinatra'
-require 'sinatra/reloader'
-
-number = rand(100)
-
-def check_guess(guess, number)
-  if (guess - number) > 5
-    "Way too high!"
-  elsif guess > number
-    "Too high!"
-  elsif (number - guess) > 5
-    "Way too low!"
-  elsif guess < number
-    "Too low."
-  elsif guess == number
-    "You are correct!
-    <p>The secret number is #{number}.</p>"
-  end
-end
-
-get '/' do
-  guess = params['guess'].to_i
-  message = check_guess(guess, number)
-  erb :index, :locals => {:number  => number,
-    :message => message }
-end
+# number = rand(100)
+#
+# def check_guess(guess, number)
+#   if (guess - number) > 5
+#     "Way too high!"
+#   elsif guess > number
+#     "Too high!"
+#   elsif (number - guess) > 5
+#     "Way too low!"
+#   elsif guess < number
+#     "Too low."
+#   elsif guess == number
+#     "You are correct!
+#     <p>The secret number is #{number}.</p>"
+#   end
+# end
+#
+# get '/' do
+#   guess = params['guess'].to_i
+#   message = check_guess(guess, number)
+#   erb :index, :locals => {:number  => number,
+#     :message => message }
+# end
 
 
 
 
-# V1
+
+
+
+
+# # I1
 
 # require 'sinatra'
 # require 'sinatra/reloader'
