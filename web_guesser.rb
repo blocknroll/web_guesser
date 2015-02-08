bdbe893852cf9f4db6df27f60ceb4e68254ff4b5
Fred Block <info@fredblock.com>
2015-02-08 10:34:45 -0700

    Refactor feedback messages.

--- web_guesser.rb
+++ web_guesser.rb
@@ -3,6 +3,7 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
+
 secret_number = rand(100)
 
 @@guesses = 5
@@ -17,31 +18,26 @@ def check_guess(guess, secret_number)
   guess = guess.to_i
 
   @@guesses -= 1
-  
 
   if (guess - secret_number) > 5
-    # @@guesses -= 1
-    {"message"    => "WAY TOO HIGH!<br>&#8595; &#8595; &#8595;",
+    {"message"    => "WAY TOO HIGH!<br>&#8595; &#8595; &#8595; &#8595; &#8595;",
      "background" => "red"}
 
   elsif guess > secret_number
-    # @@guesses -= 1
-    {"message"    => "Too high!<br>&#8595;",
+    {"message"    => "Too high!<br>&#8595; &#8595; &#8595;",
      "background" => "orange"}
 
   elsif (secret_number - guess) > 5
-    # @@guesses -= 1
-    {"message"    => "WAY TOO LOW!<br>&#8593; &#8593; &#8593;",
+    {"message"    => "&#8593; &#8593; &#8593; &#8593; &#8593;<br>WAY TOO LOW!",
      "background" => "red"}
 
   elsif guess < secret_number
-    # @@guesses -= 1
-    {"message"    => "Too low!<br>&#8593;",
+    {"message"    => "&#8593; &#8593; &#8593;<br>Too low!",
      "background" => "orange"}
 
   elsif guess == secret_number
-    {"message"    => "CORRECTOMUNDO, BRAH! <h2>
-                      #{secret_number} is the secret number!</h2>",
+    {"message"    => "CORRECTOMUNDO, BRAH!
+                      <h2>#{secret_number} is the secret number!</h2>",
      "background" => "green"}
   end
 end
@@ -53,13 +49,4 @@ get '/' do
   erb :index, :locals => {:secret_number => secret_number,
                           :guesses       => @@guesses,
                           :messages      => messages }
-
-  # if @@guesses > 0
-  #
-  # elsif @@guesses = 0
-  #   number = rand(100)
-  #   @@guesses = 5
-  #   {"message" => "You lose. A new number has been generated for you...", "background" => "red"}
-  #   # puts "You lose. A new number has been generated for you..."
-  # end
 end
